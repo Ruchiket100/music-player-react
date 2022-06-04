c5767e9c78a63626a4ae1468bc960fd7276fda3b
Ruchiket Borse <RuchiketBorse1@gmail.com>
2022-06-04 15:01:26 +0530

    types of commponent

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-  React.StrictMode >
-  <
-  App / >
-  <
-  /React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+
+
+// components -
+// stateless
+// stateful (or both) - React-createClass & ES2015 class
+
+// functional component cannot have state within
+// React props are immutable means they cannot cahnge once they are declared by main parent 
+// only when parent element change then the react will render part which is change by new props over old
+
